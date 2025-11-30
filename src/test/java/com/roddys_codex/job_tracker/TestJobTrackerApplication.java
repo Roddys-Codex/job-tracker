ca753f49c1170bd52eda10e0cba1dfd5f4233781
Roddys-Codex <daniel.kevin.roddy@gmail.com>
2025-11-30 10:39:23 +0000

    Initial repository setup.

---
+++ src/test/java/com/roddys_codex/job_tracker/TestJobTrackerApplication.java
@@ -0,0 +1,11 @@
+package com.roddys_codex.job_tracker;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestJobTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(JobTrackerApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
